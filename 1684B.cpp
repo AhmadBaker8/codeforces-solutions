3f9f1f852733f38d6894ab4a9a0dc62d021a5635
ahmad baker <133532346+AhmadBaker8@users.noreply.github.com>
2024-05-10 16:18:57 +0300

    Create 1684B.cpp

---
+++ 1684B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define Baker ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define cin(a, n) for(ll i = 0; i < n; i++) cin >> a[i];
+#define yn f?cout<<"YES\n":cout<<"NO\n";
+#define ys cout << "YES" <<endl;
+#define no cout << "NO" <<endl;
+using namespace std;
+
+
+int main() {
+    Baker
+
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+       int a,b,c;
+       cin>>a>>b>>c;
+       cout<<(ll)a+b+c<<" "<<(ll)b+c<<" "<<c<<endl;
+
+    }
+}
+
+
+ 
