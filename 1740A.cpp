11b05b0306964959d89765388f123bf400c899e6
ahmad baker <133532346+AhmadBaker8@users.noreply.github.com>
2024-05-10 15:25:27 +0300

    Create 1740A.cpp

---
+++ 1740A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define Baker ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define cin(a, n) for(ll i = 0; i < n; i++) cin >> a[i];
+#define yn f?cout<<"YES\n":cout<<"NO\n";
+#define ys cout << "YES" <<endl;
+#define no cout << "NO" <<endl;
+using namespace std;
+
+
+int main() {
+
+    Baker
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n == 2)cout << 7 << endl;
+        else cout << 3 << endl;
+    }
+
+}
+
+
+ 
